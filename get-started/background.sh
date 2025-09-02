78b9ff1533cbc8363e67aa30299091ff93e78739
adaghoui <azzeddine.daghoui@gmail.com>
2025-09-02 22:06:50 +0000

    test

--- get-started/background.sh
+++ get-started/background.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -e
 
-# Install dependencies
+# Update package lists and install dependencies
 apt-get update -y
 apt-get install -y wget unzip
 
-# Install Terraform
-TERRAFORM_VERSION="1.9.5"
+# Download and install the latest Terraform version
+TERRAFORM_VERSION="1.13.1"
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# Extract the Terraform binary, overwrite if it exists
+unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# Move Terraform binary to system PATH
 mv terraform /usr/local/bin/
+
+# Verify Terraform installation
 terraform -version
+
+# Create Terraform working directory
+mkdir -p /root/terraform
