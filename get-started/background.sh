26659b2d753ee15057af2e179a8a4850718861da
adaghoui <azzeddine.daghoui@gmail.com>
2025-09-02 22:16:01 +0000

    test

--- get-started/background.sh
+++ get-started/background.sh
@@ -9,11 +9,15 @@ apt-get install -y wget unzip
 TERRAFORM_VERSION="1.13.1"
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
+# Remove any existing terraform binary in current directory
+rm -f terraform
+
 # Unzip Terraform binary and overwrite if exists
 unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
-# Move Terraform binary to /usr/local/bin
+# Move Terraform binary to /usr/local/bin and ensure executable
 mv terraform /usr/local/bin/
+chmod +x /usr/local/bin/terraform
 
 # Check Terraform version
 terraform -version
